3b16b40a4bc78c2dd7bdaf6d595e2f2eea4f58e5
Craig Arnold <arnocr01@gmail.com>
2019-01-31 17:56:17 -0600

    change to leaderboard view to auto split response to avoid 2048 character cap

--- views/leaderboard.js
+++ views/leaderboard.js
@@ -3,18 +3,35 @@ const log = require('../utils/log.js')
 const cast = require('../utils/cast.js')
 const colors = require('../utils/colors.js')
 
-exports.render = (req, maxdkp, tallies) => {
+exports.render = async (req, maxdkp, tallies) => {
     log.view('rendering view: leaderboard')
     tallies.sort(function (a, b) { return b.value - a.value })
-    let prettyTally = ''
-    tallies.forEach(function (x, i) {
-        if (i > 50) return  //temp fix to block >2048 char error. do something better like send multiple messages
-        let name = `${isNaN(x.name) == true ? x.name : cast.user(x.name)}`
-        prettyTally += `${i + 1}) ${name}: ${parseInt(x.value / maxdkp * 100)}% (${x.value}/${maxdkp})\n`
+    cap = 60
+    chunkCount = 1
+
+    if (tallies.length > cap) {
+        chunkCount = ~~(tallies.length / cap) + 1
+    }
+    chunkSize = tallies.length / chunkCount
+    chunks = []
+    embeds = []
+    c = chunkCount
+    while (c--) {
+        chunks.unshift(tallies.slice(c * chunkCount, c * chunkCount + chunkSize))
+    }
+    chunks.forEach((chunk, num) => {
+        let prettyTally = ''
+        chunk.forEach((x, i) => {
+            let name = `${isNaN(x.name) == true ? x.name : cast.user(x.name)}`
+            prettyTally += `${((num * chunkCount) + i + 1).toString().padStart(2, '0')} ${name}: ${parseInt(x.value / maxdkp * 100)}% (${x.value}/${maxdkp})\n`
+        })
+        let embed = new RichEmbed()
+            .setColor(colors.lightblue)
+            .setDescription(`${(num == 0) ? '**leaderboard:**\n' : ''}${prettyTally}`)
+        embeds.push(embed)
     })
-    const embed = new RichEmbed()
-        .setColor(colors.lightblue)
-        .setDescription(`**leaderboard:**\n` +
-            `${prettyTally}`)
-    return req.message.channel.send(embed)
+    const promises = embeds.map(async (x) => {
+        await req.message.channel.send(x)
+    })
+    await Promise.all(promises)
 }
